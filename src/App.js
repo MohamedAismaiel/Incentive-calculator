3fc965f26c15776afe49227cfd9b87b1b1548fa4
Mohamed A.Ismaiel <mohamedismmaiel@gmail.com>
2022-07-18 21:53:46 +0200

    adding currency formatter

--- src/App.js
+++ src/App.js
@@ -51,6 +51,15 @@ function App() {
   const achievedMoneyCss = achievedMoneyIsInvalid
     ? "incentiveForm-firstLineContainer-hoursInputContainer-inputContainer-input invalid"
     : "incentiveForm-firstLineContainer-hoursInputContainer-inputContainer-input";
+  const formatter = new Intl.NumberFormat("ar-EG", {
+    style: "currency",
+    currency: "EGP",
+
+    // These options are needed to round to whole numbers if that's what you want.
+    //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+    maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+  });
+
   return (
     <div className="formContainer">
       <form className="incentiveForm">
@@ -151,8 +160,7 @@ function App() {
         {!salaryIsInvalid && !achievedMoneyIsInvalid && showOutput && (
           <div className="outputContainer">
             <p className="outputContainer-text">: الحوافز المتوقعه </p>
-            <p className="outputContainer-output">{output.toFixed(0)}</p>
-            <p className="outputContainer-currency">ج.م</p>
+            <p className="outputContainer-output">{formatter.format(output)}</p>
           </div>
         )}
         <ul className="copyrightText">
